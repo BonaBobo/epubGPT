9f26aea2a9a0437c33bdc6c19230b9bf4e12f856
Frederik Calsius <frederikcalsius@MacBook-Pro-van-Frederik.local>
2023-03-29 13:25:20 +0200

    initial commit for gradio app to chat w/ book

---
+++ src/main.py
@@ -0,0 +1,36 @@
+"""main.py."""
+import os
+
+import gradio as gr
+from dotenv import load_dotenv
+from llama_index import GPTSimpleVectorIndex, download_loader
+
+load_dotenv()
+
+book = r"path_to_book"
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+
+def predict(query: str) -> str:
+    """
+    Predict function for the chatbot.
+
+    :param input: input question for the chatbot
+    :type input: string
+    :return: response of the query
+    :rtype: string
+    """
+    response = index.query(query, similarity_top_k=1)
+    return response
+
+
+if __name__ == "__main__":
+    EpubReader = download_loader("EpubReader")
+
+    title = "Chat with the Glucose Goddess 🧜‍♀️"
+    loader = EpubReader()
+    documents = loader.load_data(file=book)
+
+    index = GPTSimpleVectorIndex(documents)
+
+    gr.Interface(fn=predict, title=title, inputs=["text"], outputs=["text"]).launch(share=False)
